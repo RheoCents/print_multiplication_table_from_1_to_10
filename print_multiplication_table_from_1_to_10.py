b34398d182dca32abb9dabd9f3eeb06ee61b2656
unknown <centinaraphael0157@gmail.com>
2024-01-24 23:21:15 +0800

    trying loop inside of a if inside of loop

--- print_multiplication_table_from_1_to_10.py
+++ print_multiplication_table_from_1_to_10.py
@@ -2,4 +2,7 @@
 range_of_numbers = [1,2,3,4,5,6,7,8,9,10]
 
 for i in range (10):
-    print (range_of_numbers[i], end=' ')
+    if i == 1:
+        for j in range (1, 11):
+            print (i*j, end=' ')
+            
