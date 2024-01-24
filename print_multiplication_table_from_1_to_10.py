63f4e78ed533a7b4eb712822aa63d533ecbb4746
unknown <centinaraphael0157@gmail.com>
2024-01-24 23:40:10 +0800

    Working code (I found where can I insert the new line)

--- print_multiplication_table_from_1_to_10.py
+++ print_multiplication_table_from_1_to_10.py
@@ -1,33 +1,42 @@
 #Print multiplication table from 1 to 10
-
+print('For this we will show the multiplication table up to 10 in python\n')
 for i in range (11):
     if i == 1:
         for j in range (1, 11):
             print (i*j, end=' ')
     if i == 2:
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
     if i == 3:
+        print('')
         for j in range (1, 11):
             print (i*j, end=' ')
     if i == 4:
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
     if i == 5:
+        print('')
         for j in range (1, 11):
             print (i*j, end=' ')
     if i == 6:
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
     if i == 7:
+        print('')
         for j in range (1, 11):
             print (i*j, end=' ')       
     if i == 8:
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
     if i == 9:
+        print('')
         for j in range (1, 11):
             print (i*j, end=' ')
     if i == 10:
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
