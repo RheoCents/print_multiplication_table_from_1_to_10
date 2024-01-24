d92cbdc99b321c6bdbc6616aea3f45f374458aac
RheoCents <150512072+RheoCents@users.noreply.github.com>
2024-01-25 01:47:25 +0800

    deleted the new lines

--- print_multiplication_table_from_1_to_10.py
+++ print_multiplication_table_from_1_to_10.py
@@ -5,38 +5,38 @@ for i in range (11):
         for j in range (1, 11):
             print (i*j, end=' ')
     if i == 2:
-        print('\n')
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
     if i == 3:
-        print('\n')
+        print('')
         for j in range (1, 11):
             print (i*j, end=' ')
     if i == 4:
-        print('\n')
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
     if i == 5:
-        print('\n')
+        print('')
         for j in range (1, 11):
             print (i*j, end=' ')
     if i == 6:
-        print('\n')
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
     if i == 7:
-        print('\n')
+        print('')
         for j in range (1, 11):
             print (i*j, end=' ')       
     if i == 8:
-        print('\n')
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
     if i == 9:
-        print('\n')
+        print('')
         for j in range (1, 11):
             print (i*j, end=' ')
     if i == 10:
-        print('\n')
+        print('')
         for j in range (1,11):
             print (i*j, end=' ')
